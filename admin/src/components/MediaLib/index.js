0ab9a5b66ffefb13d6eacb8e9d8880825ce25e5d
Souf G <3215399+gsouf@users.noreply.github.com>
2023-05-28 18:00:45 +0200

    handle mp4 as images

--- admin/src/components/MediaLib/index.js
+++ admin/src/components/MediaLib/index.js
@@ -7,11 +7,24 @@ const MediaLib = ( { isOpen, onChange, onToggle } ) => {
   const MediaLibraryDialog = components[ 'media-library' ];
 
   const handleSelectAssets = files => {
-    const formattedFiles = files.map(f => ( {
-      alt: f.alternativeText || f.name,
-      url: prefixFileUrlWithBackendUrl( f.url ),
-      mime: f.mime,
-    } ) );
+    const formattedFiles = files.map(f => {
+      let mime = f.mime;
+      let isMp4 = false;
+
+      // This is a workaround to allow mp4 files to be considered as images.
+      // CKE will add a markdown image syntax using the video url.
+      // We can handle them later using the Markdown parser.
+      if (f.mime === 'video/mp4') {
+        mime = 'image/png';
+        isMp4 = true;
+      }
+
+      return {
+        alt: `${isMp4 ? '!videofile!' : ''}${f.alternativeText || f.name}`,
+        url: prefixFileUrlWithBackendUrl(f.url),
+        mime: mime
+      };
+    });
 
     onChange( formattedFiles );
   };
